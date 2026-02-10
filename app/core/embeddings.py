6a3c9b2430bcb2679bdbe6f839ce9d310ed3022c
Shivam Sharma <shivamsharma242424@gmail.com>
2026-02-10 15:20:55 +0530

    Jina Embedding model integrated

--- app/core/embeddings.py
+++ app/core/embeddings.py
@@ -9,14 +9,44 @@
 # )
 
 #USING LOCAL EMBEDDINGS INSTEAD
-from sentence_transformers import SentenceTransformer
+# from sentence_transformers import SentenceTransformer
 
-# Load model once (this happens at server start)
-model = SentenceTransformer("all-MiniLM-L6-v2")
+# # Load model once (this happens at server start)
+# model = SentenceTransformer("all-MiniLM-L6-v2")
 
-def embed_documents(texts: list[str]) -> list[list[float]]:
-    """
-    Convert list of text chunks into vectors using a local model.
-    """
-    return model.encode(texts, show_progress_bar=False).tolist()
+# def embed_documents(texts: list[str]) -> list[list[float]]:
+#     """
+#     Convert list of text chunks into vectors using a local model.
+#     """
+#     return model.encode(texts, show_progress_bar=False).tolist()
 
+
+#USING JINA EMBEDDINGS INSTEAD
+import os
+import requests
+from typing import List
+
+JINA_API_KEY = os.getenv("JINA_API_KEY")
+
+JINA_URL = "https://api.jina.ai/v1/embeddings"
+
+HEADERS = {
+    "Authorization": f"Bearer {JINA_API_KEY}",
+    "Content-Type": "application/json"
+}
+
+def embed_documents(texts: List[str]) -> List[List[float]]:
+    response = requests.post(
+        JINA_URL,
+        headers=HEADERS,
+        json={
+            "model": "jina-embeddings-v2-base-en",
+            "input": texts
+        },
+        timeout=60
+    )
+
+    response.raise_for_status()
+    data = response.json()
+
+    return [item['embedding'] for item in data['data']]
